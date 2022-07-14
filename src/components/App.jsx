8ad7dc77fdd72b99ed4e8e90e33224b83c8db861
Anisimov Ilya <anisimov_ilya26@mail.ru>
2022-07-14 15:54:55 +0500

    create shopping-cart

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import CountersList from './CountersList';
+// import Counter from "./counter";
+
+function App() {
+	return (
+		<CountersList/>
+	);
+}
+
+export default App;
---
+++ src/components/CountersList.jsx
@@ -0,0 +1,45 @@
+import React,{useState} from 'react'
+import Counter from './counter'
+
+const CountersList = () => {
+	const initialState = [
+		{ id:0, value:0, name:'Ненужная вещь'},
+		{ id:1, value:4, name:'Ложка'},
+		{ id:2, value:0, name:'Вилка' },
+		{ id:3, value:0, name:'Тарелка' },
+		{ id:4, value:0, name:'Набор минималиста' },
+	]
+	const [counters,setCounters] = useState(initialState);
+	const handleDelete = (id) => {
+		setCounters(counters.filter((counter)=>counter.id !== id))
+	}
+	const handleReset = () => {
+		setCounters(initialState);
+	}
+	const handleIncrement = (id) => {
+		setCounters(counters.map((count)=>{
+			if(count.id === id){
+				count.value += 1;
+			}
+			return count
+		}))
+	}
+	const handleDecrement = (id) => {
+		setCounters(counters.map((count)=>{
+			if(count.id === id){
+				count.value -= 1;
+			}
+			return count
+		}))
+	}
+	return (
+		<>
+			{counters.map(count =>
+				<Counter {...count} key={count.id} onDelete={handleDelete} onIncrement={handleIncrement} onDecrement={handleDecrement}/>
+			)}
+			<button className="btn btn-primary btn-sm m-2" onClick={handleReset}>Сброс</button>
+		</>
+  )
+}
+
+export default CountersList
